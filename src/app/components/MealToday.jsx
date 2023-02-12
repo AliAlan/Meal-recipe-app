ed866719a2795ec5dd23b4a96b5e43437e1686f3
ALI HAYDAR ALAN <alanalihaydar@gmail.com>
2023-02-12 22:10:37 +0300

    applying fetch

--- src/app/components/MealToday.jsx
+++ src/app/components/MealToday.jsx
@@ -1,3 +1,15 @@
-export const MealToday = ({ result }) => {
-  return <div>{result.strMeal}</div>;
+import Image from "next/image";
+
+export const MealToday = ({ data }) => {
+  return (
+    <div className="flex">
+      <Image
+        className="border"
+        width={290}
+        height={290}
+        src={data.image}
+      ></Image>
+      {data.title}
+    </div>
+  );
 };
